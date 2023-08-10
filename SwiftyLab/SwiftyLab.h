2a4b5949f29fe4e86216249f17ef1e87611519f6
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2023-08-10 09:39:53 +0700

    Initial Commit

---
+++ SwiftyLab/SwiftyLab.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyLab.h
+//  SwiftyLab
+//
+//  Created by Azzam Ubaidillah on 10/08/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyLab.
+FOUNDATION_EXPORT double SwiftyLabVersionNumber;
+
+//! Project version string for SwiftyLab.
+FOUNDATION_EXPORT const unsigned char SwiftyLabVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyLab/PublicHeader.h>
+
+
