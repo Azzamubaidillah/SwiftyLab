ce567f2e11183a9c9840df9a04e67e9874a99295
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2023-08-10 09:51:22 +0700

    Examples

---
+++ SwiftyLibExamples/SwiftyLibExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyLibExamplesApp.swift
+//  SwiftyLibExamples
+//
+//  Created by Azzam Ubaidillah on 10/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyLibExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
